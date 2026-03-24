ee514fd18aad14fee7d0de7d3ff2a8620d76c9c8
bitscorch <paul@bitscorch.com>
2026-03-24 14:18:10 +0200

    feat: add task data model and store
    Task interface, frontmatter parser, in-memory store
    with file watching, and configurable tasks folder.

--- src/settings.ts
+++ src/settings.ts
@@ -1,36 +1,37 @@
-import {App, PluginSettingTab, Setting} from "obsidian";
-import MyPlugin from "./main";
+import { App, PluginSettingTab, Setting } from "obsidian";
+import DoomdPlugin from "./main";
 
-export interface MyPluginSettings {
-	mySetting: string;
+export interface DoomdSettings {
+	tasksFolder: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export const DEFAULT_SETTINGS: DoomdSettings = {
+	tasksFolder: "task",
+};
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class DoomdSettingTab extends PluginSettingTab {
+	plugin: DoomdPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: DoomdPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Settings #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Tasks folder")
+			.setDesc("Folder where task files are stored")
+			.addText((text) =>
+				text
+					.setPlaceholder("task")
+					.setValue(this.plugin.settings.tasksFolder)
+					.onChange(async (value) => {
+						this.plugin.settings.tasksFolder = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
