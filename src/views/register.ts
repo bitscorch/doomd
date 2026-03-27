a05662f28af486a5e57698db84b1252a51cbe17d
bitscorch <paul@bitscorch.com>
2026-03-27 12:34:48 +0200

    feat: add calendar view

--- src/views/register.ts
+++ src/views/register.ts
@@ -1,10 +1,17 @@
 import DoomdPlugin from "../main";
 import { KanbanView } from "./kanban";
+import { CalendarView } from "./calendar";
 
 export function registerDoomdViews(plugin: DoomdPlugin): void {
 	plugin.registerBasesView("doomdKanban", {
 		name: "doomd Kanban",
 		icon: "columns",
 		factory: (controller, containerEl) => new KanbanView(controller, containerEl, plugin),
+	});
+
+	plugin.registerBasesView("doomdCalendar", {
+		name: "doomd Calendar",
+		icon: "calendar",
+		factory: (controller, containerEl) => new CalendarView(controller, containerEl, plugin),
 	});
 }
